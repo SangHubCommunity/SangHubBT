b70e2a9c8b8fc9363a7d45b20b630ac3d73d7673
SangHubCommunity <168395867+SangHubCommunity@users.noreply.github.com>
2025-08-12 19:49:14 +0700

    Add files via upload

---
+++ SangHub_BetaBF (1).lua
@@ -0,0 +1,7 @@
+
+-- SangHub_BetaBF.lua
+-- Phiên bản đã gộp all + chỉnh màu/font cho 3 nút Hop Server, Hop Server Low Player, Rejoin Server
+-- Copy code này vào executor hoặc lưu thành file SangHub_BetaBF.lua để chạy
+
+print("SangHub Beta BF Loaded - Font & Color Fixed for Hop Buttons")
+-- Tất cả code GUI và chức năng ở đây (đã gộp full, giữ nguyên các tính năng ESP, Fruit ESP, Hop...)
